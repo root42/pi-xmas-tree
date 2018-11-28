6f21a4f1b9dd0bfa0d66a337545c91d2e2404b35
Arne Schmitz <arne.schmitz@gmx.net>
2018-11-28 22:46:35 +0100

    read key only when no parameter was given

--- youtube.sh
+++ youtube.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo -n "Streaming key: "
-read -s streamingkey
-echo
+if [ $# -eq 0 ]
+then
+  echo -n "Streaming key: "
+  read -s streamingkey
+  echo
+else
+  streamingkey=$1
+fi
 raspivid -o - -awb shade -t 0 -vf -hf -fps 30 -b 1000000 |  \
 avconv -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 \
 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k \
