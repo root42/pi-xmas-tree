c9d568d85284fc5e6efadab2715bed846c5bbee5
Arne Schmitz <arne.schmitz@gmx.net>
2018-12-25 23:23:28 +0000

    add timestamp to log output

--- xmas-client.py
+++ xmas-client.py
@@ -8,6 +8,11 @@ import json
 import tornado.httpclient
 import tornado.ioloop
 import tornado.web
+import datetime
+
+def ts():
+    st = str(datetime.datetime.utcnow())
+    return "[%s] " % st
 
 tree = LEDBoard( *range(2,28), pwm=True )
 old_tree = [ 0.0 for i in range( 0, 26 ) ]
@@ -23,16 +28,16 @@ def getLedStatus():
             value = float( led[ "value" ] )
             index = int( led[ "index" ] )
             if old_tree[ index ] != value:
-                print "Setting led %u to %f" % (index, value)
+                print ts() + "Setting led %u to %f" % (index, value)
                 tree[ index ].value = value
                 old_tree[ index ] = value
     except tornado.httpclient.HTTPError as e:
         # HTTPError is raised for non-200 responses; the response
         # can be found in e.response.
-        print("Error: " + str(e))
+        print( ts() + "Error: " + str(e))
     except Exception as e:
         # Other errors are possible, such as IOError.
-        print("Error: " + str(e))
+        print( ts() + "Error: " + str(e))
     http_client.close()
 
 if __name__ == "__main__":
