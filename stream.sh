d1770bdabf9846b75e28db681a3cd70be57372c4
Arne Schmitz <arne.schmitz@gmx.net>
2021-12-24 21:28:08 +0100

    avconv has been replaced by ffmpeg, AWB is working better now

--- stream.sh
+++ stream.sh
@@ -13,7 +13,7 @@ fi
 URL="rtmp://live.twitch.tv/app/${streamingkey}"
 #URL="rtmp://a.rtmp.youtube.com/live2/${streamingkey}"
 
-raspivid -o - -awb off -awbg 1.0,1.0 -t 0 -vf -hf -fps 30 -b 1000000 |  \
-avconv -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 \
+raspivid -o - -awb auto -t 0 -vf -hf -fps 30 -b 1000000 |  \
+ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 \
 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k \
 -g 50 -strict experimental -f flv "${URL}"
